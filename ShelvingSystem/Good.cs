cbde063b17eae5d0403d467e379fc608689dd576
Tim Kainz <tikaiz@gmx.at>
2024-09-15 13:29:12 +0200

    Added Goods class

---
+++ ShelvingSystem/Good.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShelvingSystem
+{
+    internal abstract class Good
+    {
+        protected int id {  get; set; }
+        public Good(int id) => this.id = id;
+        public abstract String Description();
+        public abstract String Kurzzeichen();
+    }
+}
